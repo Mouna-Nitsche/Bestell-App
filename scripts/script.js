ac6e103fdce9be18781db4e5850a4d97e19bb3c3
Mouna Nitsche <mouna.nitsche02@googlemail.com>
2026-02-10 14:12:34 +0100

    styling all HTML Elements, using an overlay for one image. Added all needed js files and prepared them.

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+// let meals = ["Burger & Sandwiches", "Pizza", "Salad"];
+
+// function categorie() {
+//     //This Div displays all comments
+//     const categorieRef = document.getElementById("categorie_nr_1");
+//     categorieRef.innerHTML += foodMenu();
+// }
---
+++ scripts/template.js
@@ -0,0 +1,7 @@
+// function foodMenu() {
+//     //if there is 1 or more coment
+//     return `
+//                 <p>[${meals[0]}]</p>
+//                 <h3>Here</h3>
+//             `;
+// }
