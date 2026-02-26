19116715b57ef0a72b67f8f7c21d464b5bbcbe66
Mouna Nitsche <mouna.nitsche02@googlemail.com>
2026-02-26 13:02:20 +0100

    A function has been added to calculate the total amount in the basket.

--- scripts/script.js
+++ scripts/script.js
@@ -10,6 +10,8 @@
 // let dishPrice = myDishes.Dishesindex.price;
 // let dishIndex = myDishes.Dishesindex;
 
+
+// lädt und generiert die einzelnen Menu Karten
 function iterateDishes() {
     const wrapper_content_Ref = document.getElementById("menu_card_wrapper");
 
@@ -26,32 +28,48 @@ function generateFoodCard(Dishesindex) {
     
 }
 
-function checkBasket () {
-    if (warenkorb.cardItmes.value > cardItmes.length){
-
-    }
-}
-
-//calc the warenkorb.total
-function calculation() {
-    let result = warenkorb.subtotal + warenkorb.delivery_fee
-    warenkorb.total = result
-}
-
-
+//template von dem Sektionsbalken und der Warenkorb wird rein geschrieben und angezeigt
 function displayBasket() {
     let categorie_wrapperRef = document.getElementById("categorie_wrapper");
+    // calculation()
     categorie_wrapperRef.innerHTML += basketCard()
+    //Basekt wird geladen
 }
 
 
+//Schreibt den Names des Gerichts aus dem Array, in ein P Tag für menu details.
+//Fügt den Namen in das Array Warenkorb cardItems hinzu
 function addToBasket(Dishesindex) {
     let menu_basketRef = document.getElementById("menu_basket")
 
     menu_basketRef.innerHTML = "1 x " + myDishes[Dishesindex].name;
 
-    //push x into the basketDishes array
-
+    //push x´Dish Name into the basketDishes array
+    warenkorb.cardItmes.push(myDishes[Dishesindex].name)
+    checkBasket()
 }
 
-console.log(myDishes);
+
+//Überprüfen ob Warenkorb nicht leer ist, als Bedingung um die Berechnung der Items auszuführen.
+function checkBasket() {
+    if (warenkorb.cardItmes.length > 0){
+        //show the cardItems
+        calculation()
+    }
+}
+
+console.log("WK cardItme value " + warenkorb.cardItmes.length);
+
+
+//calc the warenkorb.total
+function calculation() {
+    let calcResult = warenkorb.subtotal + warenkorb.delivery_fee
+    // warenkorb.total.push(calcResult)
+    warenkorb = {total : calcResult}
+    console.log("Warenkorb Total " + warenkorb.total);
+
+
+    // push.warenkorb.total(result.value);
+}
+
+
