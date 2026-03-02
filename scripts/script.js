f803d7e44244dda59e3c268eb716b5310cc9333f
Mouna Nitsche <mouna.nitsche02@googlemail.com>
2026-03-03 00:40:10 +0100

    Deleting outdated code and unnecessary comments to maintain readability

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,3 @@
-// function categorie() {
-//     //This Div displays all comments
-//     const menu_card_Ref = document.getElementById("menu_card");
-//     menu_card_Ref.innerHTML 
-// }
-
-// global variable
-// let dishName = myDishes.Dishesindex.name;
-// let dishDescribtion = myDishes.Dishesindex.describtion;
-// let dishPrice = myDishes.Dishesindex.price;
-// let dishIndex = myDishes.Dishesindex;
-
-
 // lädt und generiert die einzelnen Menu Karten
 function iterateDishes() {
     const wrapper_content_Ref = document.getElementById("menu_card_wrapper");
@@ -18,47 +5,65 @@ function iterateDishes() {
         for (let Dishesindex = 0; Dishesindex < myDishes.length; Dishesindex++) {
         wrapper_content_Ref.innerHTML += foodCard(Dishesindex);
     }
-    // displayBasket(Dishesindex)
-
-}
-
-
-//basketDishes
-function generateFoodCard(Dishesindex) {
-    
 }
 
 //template von dem Sektionsbalken und der Warenkorb wird rein geschrieben und angezeigt
 function displayBasket() {
     let categorie_wrapperRef = document.getElementById("categorie_wrapper");
-    // calculation()
     categorie_wrapperRef.innerHTML += basketCard()
-    //Basekt wird geladen
 }
 
 
-//Schreibt den Names des Gerichts aus dem Array, in ein P Tag für menu details.
+//Schreibt den Name des Gerichts aus dem Array, in ein P Tag für menu details.
 //Fügt den Namen in das Array Warenkorb cardItems hinzu
 function addToBasket(Dishesindex) {
-    let menu_basketRef = document.getElementById("menu_basket")
+    console.log("addToBasekt Conso Log!");
 
-    menu_basketRef.innerHTML = "1 x " + myDishes[Dishesindex].name;
+    amountOf(Dishesindex)
+    renderBasketCardItem()
+}
 
-    //push x´Dish Name into the basketDishes array
-    warenkorb.cardItmes.push(myDishes[Dishesindex].name)
-    checkBasket()
+function renderBasketCardItem() {
+        let menu_basketRef = document.getElementById("menu_basket")
+    
+        //Mit der Zeile werden Werte nicht mehr Doppelt geladen ?
+        menu_basketRef.innerHTML = "";
+
+        for (let i = 0; i < warenkorb.cardItems.length; i++) {
+        let item = warenkorb.cardItems[i];
+        console.log(warenkorb.cardItems[i]);
+        
+        menu_basketRef.innerHTML += generateSingleCardItem(item);
+    }
 }
 
 
 //Überprüfen ob Warenkorb nicht leer ist, als Bedingung um die Berechnung der Items auszuführen.
-function checkBasket() {
-    if (warenkorb.cardItmes.length > 0){
-        //show the cardItems
-        calculation()
+
+console.log("WK cardItme value " + warenkorb.cardItems.length);
+
+
+function amountOf(Dishesindex) {
+    //Filterfunktion nutzen für den Namen vom Gericht
+    //Amount erhöhen
+    //arrow function funktioniert wie eine for loop sucht den Namen
+    //push x Dish Name into the basketDishes array
+
+    let foundMenu = warenkorb.cardItems.find(cardItme => cardItme.name === `${myDishes[Dishesindex].name}`)
+
+    if (foundMenu) {
+        foundMenu.amount ++;
+    } else {
+    let choosenMenu = {
+        name: myDishes[Dishesindex].name,
+        price: myDishes[Dishesindex].price,
+        amount: myDishes[Dishesindex].amount
+    };
+    warenkorb.cardItems.push(choosenMenu)
+    console.log("Gewählten Menus in CardItmes:" + warenkorb.cardItems);
+
     }
 }
-
-console.log("WK cardItme value " + warenkorb.cardItmes.length);
 
 
 //calc the warenkorb.total
@@ -68,8 +73,5 @@ function calculation() {
     warenkorb = {total : calcResult}
     console.log("Warenkorb Total " + warenkorb.total);
 
-
     // push.warenkorb.total(result.value);
 }
-
-
