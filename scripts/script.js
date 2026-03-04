d69937301486facc06b058403ead04643d09e23e
Mouna Nitsche <mouna.nitsche02@googlemail.com>
2026-03-04 02:36:14 +0100

    restructured the Code Base, spliting functions, rendering the basket, wrote the loigc: calculation for the basket. Stored values in the warenkorb Array. Added opening and closing func for the dialog

--- scripts/script.js
+++ scripts/script.js
@@ -16,32 +16,12 @@ function displayBasket() {
 
 //Schreibt den Name des Gerichts aus dem Array, in ein P Tag für menu details.
 //Fügt den Namen in das Array Warenkorb cardItems hinzu
-function addToBasket(Dishesindex) {
+function addToBasket(Dishesindex, item) {
     console.log("addToBasekt Conso Log!");
 
     amountOf(Dishesindex)
     renderBasketCardItem()
 }
-
-function renderBasketCardItem() {
-        let menu_basketRef = document.getElementById("menu_basket")
-    
-        //Mit der Zeile werden Werte nicht mehr Doppelt geladen ?
-        menu_basketRef.innerHTML = "";
-
-        for (let i = 0; i < warenkorb.cardItems.length; i++) {
-        let item = warenkorb.cardItems[i];
-        console.log(warenkorb.cardItems[i]);
-        
-        menu_basketRef.innerHTML += generateSingleCardItem(item);
-    }
-}
-
-
-//Überprüfen ob Warenkorb nicht leer ist, als Bedingung um die Berechnung der Items auszuführen.
-
-console.log("WK cardItme value " + warenkorb.cardItems.length);
-
 
 function amountOf(Dishesindex) {
     //Filterfunktion nutzen für den Namen vom Gericht
@@ -61,17 +41,50 @@ function amountOf(Dishesindex) {
     };
     warenkorb.cardItems.push(choosenMenu)
     console.log("Gewählten Menus in CardItmes:" + warenkorb.cardItems);
-
     }
 }
 
+function renderBasketCardItem() {
+        let menu_basketRef = document.getElementById("menu_basket")
+        //Mit der Zeile werden Werte nicht mehr Doppelt geladen ?
+        menu_basketRef.innerHTML = "";
 
-//calc the warenkorb.total
-function calculation() {
-    let calcResult = warenkorb.subtotal + warenkorb.delivery_fee
-    // warenkorb.total.push(calcResult)
-    warenkorb = {total : calcResult}
-    console.log("Warenkorb Total " + warenkorb.total);
+        //Überprüfen ob Warenkorb nicht leer ist, als Bedingung um die Berechnung der Items auszuführen.
 
-    // push.warenkorb.total(result.value);
+        let subtotal_sum = 0;
+
+        for (let i = 0; i < warenkorb.cardItems.length; i++) {
+        let item = warenkorb.cardItems[i];
+        console.log(warenkorb.cardItems[i]);
+        menu_basketRef.innerHTML += generateSingleCardItem(item);
+
+        let subtotal_item_calc = item.amount * item.price;
+        
+        //jede subtotal vom Gericht darauf rechnen
+        subtotal_sum += subtotal_item_calc;
+
+        console.log(warenkorb.subtotal);
+        
+    }
+    let final_total = subtotal_sum + warenkorb.delivery_fee
+    warenkorb.total = final_total;
+// }
+
+// function updateBasketTotals(subtotal_sum, final_total) {
+    document.getElementById("subtotal").innerHTML = subtotal_sum.toFixed(2) + " €";
+    document.getElementById("total_price").innerHTML = final_total.toFixed(2) + " €";
+    document.getElementById("buy_btn").innerHTML = "Buy now " + "(" + final_total.toFixed(2) + ")" + " €";
+}
+
+console.log("WK cardItme value " + warenkorb.cardItems.length);
+
+
+function openOrderDialog() {
+    const dialogRef = document.getElementById("myDialog");
+    dialogRef.showModal();
+}
+
+function closeOrderDialog() {
+    const dialogRef = document.getElementById("myDialog");
+    dialogRef.close();
 }
